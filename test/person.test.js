cc71fa97ac21bd7073ce4cda35efbecf24bc5bc1
Jorge Aguiar Martin <jorge@leanmind.es>
2023-05-27 03:20:45 +0100

    add only custom message example

--- test/person.test.js
+++ test/person.test.js
@@ -2,21 +2,30 @@ const { expect } = require('chai')
 const Person = require('../src/person')
 
 
-describe('Using Person Custom Assertions', function () {
-  it('Should fail for child when expecting an adult', function () {
-
-    const example = new Person("Peter", 11)
-    expect(example).to.be.an.adult()
+describe('Chai domain tests', function () {
+  describe('Overriding with custom message', function () {
+    it('Should show the custom message on failure', function () {
+      const example = new Person("Peter", 18)
+      expect(example).to.be.lessThan(18, 'is not a child')
+    })
   })
 
-  it('Should fail for adult when expecting a child', function () {
+  describe('Using custom assertions', function () {
+    it('Should fail for child when expecting an adult', function () {
 
-    const example = new Person("Peter", 18)
-    expect(example).to.be.a.child()
-  })
+      const example = new Person('Peter', 11)
+      expect(example).to.be.an.adult()
+    })
 
-  it('Should pass for child when expecting a child', function () {
-      const example = new Person("Peter", 11)
+    it('Should fail for adult when expecting a child', function () {
+
+      const example = new Person('Peter', 18)
       expect(example).to.be.a.child()
+    })
+
+    it('Should pass for child when expecting a child', function () {
+      const example = new Person('Peter', 11)
+      expect(example).to.be.a.child()
+    })
   })
 })
